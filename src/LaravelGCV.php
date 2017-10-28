a4b85fdb253efffeb61d943e12b16165408caa11
Giulio Prina Ricotti <giulio.prinaricotti@araundu.it>
2017-10-28 11:43:58 +0200

    colors(): return null when imageProperties is missing

--- src/LaravelGCV.php
+++ src/LaravelGCV.php
@@ -4,32 +4,33 @@ namespace LaravelGCV;
 use Google\Cloud\Vision\VisionClient;
 
 class LaravelGCV {
-	public function test(){
-		return "CIAO!!";
-	}
-
-	public function colors($file){
+	public function colors($file) {
 
 		//EXT PATH
-		if (is_string($file) && in_array(parse_url($file, PHP_URL_SCHEME), array('http','https'))) {
+		if (is_string($file) && in_array(parse_url($file, PHP_URL_SCHEME), array('http', 'https'))) {
 			$imageData = file_get_contents($file);
 		} else {
 			$imageData = $file;
 		}
 
 		$config = [
-	        'keyFilePath' => config('gcv.key_file_path'),
-	        'projectId' => config('gcv.project_id'),
-	    ];
+			'keyFilePath' => config('gcv.key_file_path'),
+			'projectId' => config('gcv.project_id'),
+		];
 
-	    $vision = new VisionClient($config);
-	    $image = $vision->image($imageData, [
-	        'IMAGE_PROPERTIES'
-	    ]);
+		$vision = new VisionClient($config);
+		$image = $vision->image($imageData, [
+			'IMAGE_PROPERTIES',
+		]);
 
-	    $result = $vision->annotate($image);
-	    $colors = $result->imageProperties()->colors();
+		$result = $vision->annotate($image);
 
-    	return $colors;
+		if (is_null($result->imageProperties())) {
+			return null;
+		}
+
+		$colors = $result->imageProperties()->colors();
+
+		return $colors;
 	}
 }
